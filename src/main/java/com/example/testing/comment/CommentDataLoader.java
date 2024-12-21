61c975f30113e69825f988b2a641c1bf7bc84b8a
Yusuf Emre Bilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-12-21 22:00:47 +0300

    chore: add testcontainers dependency and update CommentDataLoader
    * add profile configuration for CommentDataLoader

--- src/main/java/com/example/testing/comment/CommentDataLoader.java
+++ src/main/java/com/example/testing/comment/CommentDataLoader.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ import java.io.InputStream;
 import java.util.List;
 
 @Component
+@Profile("!test")
 public class CommentDataLoader implements CommandLineRunner {
 
     private final ObjectMapper mapper;
